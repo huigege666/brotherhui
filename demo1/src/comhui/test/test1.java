59b44c14ecde45ad3dee14a41c5600b848f7e5ab
huigehe <1149955247@qq.com>
2019-11-09 20:07:48 +0800

    工程修改

--- demo1/src/comhui/test/test1.java
+++ demo1/src/comhui/test/test1.java
@@ -4,5 +4,8 @@ public class test1 {
 
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
     }
 }
